5a224e3b81c4776d7da5845b3df4b1fdca0475fd
stephen <prometheus1987@protonmail.com>
2025-06-16 23:06:56 -0400

    bringing back security dashboard

--- src/config.ts
+++ src/config.ts
@@ -4,3 +4,10 @@
 export const SITE_TITLE = "SputnikOS";
 export const SITE_DESCRIPTION = "Zero-Trust & Open-Source";
 export const GENERATE_SLUG_FROM_TITLE = true;
+
+export const TRANSITION_API = true;
+export const BREACH_API = "https://haveibeenpwned.com/api/v3/latestbreach";
+export const TOP_VULN_API = "https://cvedb.shodan.io/cve/CVE-2015-0204";
+export const IP_API = "https://api.shodan.io/tools/myip?key=";
+export const STATS_API = "https://shodan.nyc3.digitaloceanspaces.com/exposure-data/US.json";
+export const GLOSSARY_API = "https://github.com/sputnikOS/library/blob/main/courses/glossary.json"
